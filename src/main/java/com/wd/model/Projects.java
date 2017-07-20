9c4023857f31b7c439d028b5d6ead14863207cc5
chenwendong <1234>
2017-07-20 18:05:08 +0800

    集成 spring

---
+++ src/main/java/com/wd/model/Projects.java
@@ -0,0 +1,16 @@
+package com.wd.model;
+
+public class Projects {
+
+	private String appid;
+	private String name;
+	private String icon;
+	private String platform;
+	private String type;
+	private String info;
+	private String time;
+	private String creator;
+	
+	
+	
+}
