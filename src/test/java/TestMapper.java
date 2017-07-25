d2c921faa2b562919714402f2a3f67235834bb89
chenwendong <1234>
2017-07-25 15:07:30 +0800

    mybatis 使用接口和xml 整合 spring 成功

--- src/test/java/TestMapper.java
+++ src/test/java/TestMapper.java
@@ -1,21 +1,24 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.wd.mapper.ProjectsMapper;
 import com.wd.service.ProjectsService;
 
 @RunWith(SpringJUnit4ClassRunner.class)  
 @ContextConfiguration(locations="classpath:spring.xml")  
 public class TestMapper  extends AbstractJUnit4SpringContextTests{
 	
-	private ProjectsService projectsService;
+	@Autowired
+	private ProjectsMapper projectsMapper;
 	
 	@Test
 	public void test(){
 		System.out.println("test");
-//		projectsService.queryById("111");
+		projectsMapper.selectOne("002");
 	}
 	
 }
